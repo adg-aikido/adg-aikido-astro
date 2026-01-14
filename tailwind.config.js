b7f61ffa8f187dd03203351712bed7532344034a
Vlad Dumitrescu <vladdu55@gmail.com>
2026-01-14 16:16:16 +0100

    Updates styles and adds navigation
    Refactors the styling system to use Tailwind CSS and improves overall visual consistency by adjusting colors and adding a new navigation component. This change aims to enhance the user experience.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    theme: {
+        colors: {
+            gray: colors.slate,
+            transparent: colors.transparent,
+            white: colors.white,
+            black: colors.black,
+            primary: colors.blue,
+            accent: colors.violet,
+            error: colors.red,
+            ok: colors.green,
+        },
+    },
+};
