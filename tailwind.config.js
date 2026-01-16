816f1d0ca428541af9cff4d538fdece47675f4d0
Vlad Dumitrescu <vladdu55@gmail.com>
2026-01-16 20:31:33 +0100

    Convert styling to DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,5 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-    theme: {
-        colors: {
-            gray: colors.slate,
-            transparent: colors.transparent,
-            white: colors.white,
-            black: colors.black,
-
-            primary: colors.sky,
-            secondary: colors.emerald,
-            accent: colors.amber,
-
-            error: colors.red,
-            ok: colors.green,
-            info: colors.blue,
-        },
-    },
+  daisyui: {
+    logs: false,
+  },
 };
-
